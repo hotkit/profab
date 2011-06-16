067f89b08b4e0d674f8efe099b314629a80a677d
Kirit Sælensminde <kirit@felspar.com>
2011-06-16 22:24:10 +0700

    Added in code to merge a configuration into the Python configuration object.
    --HG--
    branch : experiment

--- profab/__init__.py
+++ profab/__init__.py
@@ -8,9 +8,15 @@ class _Configuration(object):
         """Load the configuration for the specified client.
         """
         # Set up the default configuration
+        self.client = client
         self.host = 'ec2'
         self.keys = _Keys(api = 'test-api-key',
             secret = 'test-api-secret')
+        overrides = self.load_configuration()
+        _merge_attrs(self, overrides)
+
+    def load_configuration(self):
+        return {}
 
 
 class _Keys(object):
@@ -22,3 +28,14 @@ class _Keys(object):
         """
         for k, v in kwargs.items():
             setattr(self, k, v)
+
+
+def _merge_attrs(host, attrs):
+    for k, v in attrs.items():
+        if hasattr(v, 'items'):
+            if not hasattr(host, k):
+                setattr(host, k, _Keys(**v))
+            else:
+                _merge_attrs(getattr(host, k), v)
+        else:
+            setattr(host, k, v)
