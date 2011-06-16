47a399f7532ed029cd294b42b14731e9080014cf
Kirit Sælensminde <kirit@felspar.com>
2011-06-16 17:07:11 +0700

    Test the default configuration.
    --HG--
    branch : experiment

--- profab/__init__.py
+++ profab/__init__.py
@@ -7,3 +7,18 @@ class _Configuration(object):
     def __init__(self, client):
         """Load the configuration for the specified client.
         """
+        # Set up the default configuration
+        self.host = 'ec2'
+        self.keys = _Keys(api = 'test-api-key',
+            secret = 'test-api-secret')
+
+
+class _Keys(object):
+    """Used for storage of keys.
+    """
+
+    def __init__(self, **kwargs):
+        """Load the specified key values.
+        """
+        for k, v in kwargs.items():
+            setattr(self, k, v)
--- profab/tests/test_configuration.py
+++ profab/tests/test_configuration.py
@@ -5,3 +5,6 @@ class Configuration(TestCase):
     def test_load_config(self):
         from profab import _Configuration
         config = _Configuration('kirit')
+        self.assertEquals(config.host, 'ec2')
+        self.assertEquals(config.keys.api, 'test-api-key')
+        self.assertEquals(config.keys.secret, 'test-api-secret')
