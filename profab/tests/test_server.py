8dc20a44fa2d96a188d1ae58285e68f167cf55f2
Kirit Sælensminde <k@kirit.com>
2011-06-22 22:02:59 +0700

    Got a baseline set of mocks used when starting a server.

--- profab/tests/test_server.py
+++ profab/tests/test_server.py
@@ -1,13 +1,23 @@
 from unittest2 import TestCase
 import mock
 
-from profab.tests.mockboto.connection import ServerCnx, AuthnCnx
+from fabric.api import env
+from fabric.state import connections
 
 from profab.server import Server
 
+from profab.tests.mockboto.connection import ServerCnx, AuthnCnx
+
+
+def _start_connection(*args):
+    print env.host_string
+    connections['ubuntu@%s:22' % env.host_string] = True
 
 class ServerLifecycle(TestCase):
     @mock.patch('profab.authentication.EC2Connection', AuthnCnx)
     @mock.patch('profab.server.EC2Connection', ServerCnx)
+    @mock.patch('profab.server.reboot', _start_connection)
+    @mock.patch('profab.server.sudo', _start_connection)
+    @mock.patch('time.sleep', lambda s: None)
     def test_start_basic(self):
         server = Server.start('test')
