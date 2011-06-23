a72e72781ebb21c6999abdc105fc5367e9291df6
Kirit Sælensminde <k@kirit.com>
2011-06-23 09:29:11 +0700

    Added a test for the key creation path of the authentication handling.

--- profab/tests/mockboto/connection.py
+++ profab/tests/mockboto/connection.py
@@ -14,7 +14,22 @@ class Cnx(object):
 
 
 class  AuthnCnx(Cnx):
-    pass
+    def __init__(self, *args):
+        super(AuthnCnx, self).__init__(*args)
+        self.key_pairs = []
+        self.key_pairs_created = []
+
+    def get_all_key_pairs(self):
+        if self.key_pairs:
+            return self.key_pairs
+        else:
+            return super(AuthnCnx, self).get_all_key_pairs()
+    
+    def create_key_pair(self, host):
+        self.key_pairs_created.append(host)
+        key_pair = _Keys(name=host)
+        key_pair.save = lambda f: None
+        return key_pair
 
 
 class ServerCnx(Cnx):
