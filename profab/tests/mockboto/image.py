7a26787f20b8304310baf218313b7ef4afe56ebd
Kirit Sælensminde <k@kirit.com>
2011-06-23 10:03:33 +0700

    Get a full coverage in the server start up code.

--- profab/tests/mockboto/image.py
+++ profab/tests/mockboto/image.py
@@ -1,6 +1,15 @@
 from profab import _Keys
 
 
+class MockInstance(object):
+    def __init__(self, state):
+        self.state = state
+        self.dns_name='hostname'
+    
+    def update(self):
+        self.state = 'running'
+
+
 class MockImage(object):
     def run(self, instance_type, key_name, security_groups):
-        return _Keys(instances=[_Keys(state='running', dns_name='hostname')])
+        return _Keys(instances=[MockInstance('pending')])
