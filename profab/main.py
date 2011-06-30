89753877c7531d30dd0377f1cd4a4f5b952938f8
Kirit Sælensminde <k@kirit.com>
2011-07-01 00:49:37 +0700

    Basic parsing of command line now works.

--- profab/main.py
+++ profab/main.py
@@ -6,7 +6,15 @@
 def process_arguments(*args):
     """Convert the arguments into a list of commands or options and values.
     """
+    args = list(args) # Convert tuple to list
+    args.reverse() # We really wanted head() here...
     instructions = []
-    for arg in args:
-        instructions.append(arg)
+    while len(args):
+        print args
+        head = args.pop()
+        print args, head
+        if head.startswith('--'):
+            instructions.append((head[2:], args.pop()))
+        else:
+            instructions.append((head,))
     return instructions
--- profab/tests/test_main.py
+++ profab/tests/test_main.py
@@ -10,4 +10,8 @@ class TestArguments(TestCase):
 
     def test_one_command(self):
         commands = process_arguments('postgres')
-        self.assertEquals(commands, ['postgres'])
+        self.assertEquals(commands, [('postgres',)])
+
+    def test_one_configuration(self):
+        commands = process_arguments('--monitor', 'localhost')
+        self.assertEquals(commands, [('monitor', 'localhost')])
