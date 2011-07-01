a8d39c9767866bf5d767c9c87d96b569e9578f4d
Kirit Sælensminde <k@kirit.com>
2011-07-01 10:11:22 +0700

    The default Munin configuration now works monitoring itself.

--- profab/role/munin.py
+++ profab/role/munin.py
@@ -1,5 +1,12 @@
 """Sets up and configures Munin on the machine
 """
+from cStringIO import StringIO
+
+from fabric.api import settings, sudo
+from fabric.contrib.files import exists
+from fabric.operations import put
+
+from profab import _logger
 from profab.role import Role
 
 
@@ -7,6 +14,26 @@ class AddRole(Role):
     """Adds the default Munin configuration.
     """
     packages = ['apache2', 'munin', 'munin-plugins-extra']
+    
+    def configure(self):
+        """Adds the Munin web site to Apache.
+        """
+        if exists("/etc/apache2/sites-enabled/000-default"):
+            _logger.warning("Removing Apache2 default site")
+            sudo("a2dissite 000-default")
+
+        _logger.info("Configure Apache2 for Munin")
+        config_file = "/etc/apache2/sites-available/munin.conf"
+        if not exists(config_file):
+            config = StringIO()
+            config.write("""
+<VirtualHost *:80>
+    DocumentRoot /var/cache/munin/www
+</VirtualHost>
+""")
+            put(config, config_file, use_sudo=True)
+            sudo('a2ensite munin.conf')
+            sudo('service apache2 reload')
 
 
 class Configure(Role):
@@ -14,3 +41,13 @@ class Configure(Role):
     """
     packages = [ 'munin-node', 'munin-plugins-extra']
 
+    def configure(self):
+        """Add the node configuration to the server, and the server
+        configuration on the node.
+        """
+        _logger.info("Configuring node %s", self.server)
+        sudo("service munin-node restart")
+
+        with settings(host_string=self.parameter):
+            _logger.info("Configuring server %s", self.parameter)
+            sudo("service apache2 reload")
