d9a84249e1e4c343749ce62eeed0b1e7085aa13e
Kirit Sælensminde <k@kirit.com>
2011-07-06 09:14:37 +0700

    Re-ordered the mocks so they're alphabetical.

--- profab/tests/test_server.py
+++ profab/tests/test_server.py
@@ -10,82 +10,82 @@ from profab.tests.mockfabric.connections import start_connection
 
 
 class ServerLifecycle(TestCase):
+    @mock.patch('os.mkdir', lambda p: None)
     @mock.patch('profab.server.EC2Connection', MockConnection)
     @mock.patch('profab.server.append', start_connection)
     @mock.patch('profab.server.reboot', start_connection)
     @mock.patch('profab.server.run', start_connection)
     @mock.patch('profab.server.sudo', start_connection)
     @mock.patch('time.sleep', lambda s: None)
-    @mock.patch('os.mkdir', lambda p: None)
     def test_start_basic(self):
         server = Server.start('test')
         self.assertEquals(str(server),
             u"ec2-host (host) [default] {}")
 
 
+    @mock.patch('os.mkdir', lambda p: None)
     @mock.patch('profab.server.EC2Connection', MockConnection)
     @mock.patch('profab.server.append', start_connection)
     @mock.patch('profab.server.reboot', start_connection)
     @mock.patch('profab.server.run', start_connection)
     @mock.patch('profab.server.sudo', start_connection)
     @mock.patch('time.sleep', lambda s: None)
-    @mock.patch('os.mkdir', lambda p: None)
     def test_start_with_role(self):
         server = Server.start('test', 'postgres')
         self.assertEquals(str(server),
             u"ec2-host (host) [default] {}")
 
 
+    @mock.patch('os.mkdir', lambda p: None)
     @mock.patch('profab.server.EC2Connection', MockConnection)
     @mock.patch('profab.server.append', start_connection)
     @mock.patch('profab.server.reboot', start_connection)
     @mock.patch('profab.server.run', start_connection)
     @mock.patch('profab.server.sudo', start_connection)
     @mock.patch('time.sleep', lambda s: None)
-    @mock.patch('os.mkdir', lambda p: None)
     def test_connect_and_upgrade(self):
         server = Server.connect('test', 'ec2-host')
         server.dist_upgrade()
 
 
+    @mock.patch('os.mkdir', lambda p: None)
     @mock.patch('profab.server.EC2Connection', MockConnection)
     @mock.patch('time.sleep', lambda s: None)
-    @mock.patch('os.mkdir', lambda p: None)
     def test_connect_and_terminate(self):
         server = Server.connect('test', 'ec2-host')
         server.terminate()
 
 
+    @mock.patch('os.mkdir', lambda p: None)
     @mock.patch('profab.server.EC2Connection', MockConnection)
     @mock.patch('profab.server.sudo', start_connection)
-    @mock.patch('os.mkdir', lambda p: None)
     def test_connect_and_add_role(self):
         server = Server.connect('test', 'ec2-host')
         server.add_role('postgres')
 
 
+    @mock.patch('os.mkdir', lambda p: None)
     @mock.patch('profab.role.munin.exists', lambda f: True)
     @mock.patch('profab.role.munin.put', start_connection)
     @mock.patch('profab.role.munin.run', start_connection)
     @mock.patch('profab.role.munin.sudo', start_connection)
     @mock.patch('profab.server.EC2Connection', MockConnection)
     @mock.patch('profab.server.sudo', start_connection)
-    @mock.patch('os.mkdir', lambda p: None)
     def test_connect_and_configure(self):
         server = Server.connect('test', 'ec2-host')
         server.add_role('munin')
         server.add_role('munin', 'monitor.example.com')
 
 
-    @mock.patch('profab.server.EC2Connection', MockConnection)
     @mock.patch('os.mkdir', lambda p: None)
+    @mock.patch('profab.server.EC2Connection', MockConnection)
     def test_try_connect_to_invalid_host(self):
         server = Server.connect('test', 'not-a-host')
         self.assertIs(server, None)
 
+    @mock.patch('os.mkdir', lambda p: None)
     @mock.patch('profab.server.EC2Connection', MockConnection)
     @mock.patch('time.sleep', lambda s: None)
-    @mock.patch('os.mkdir', lambda p: None)
     def test_server_role_not_found(self):
         server = Server.connect('test', 'ec2-host')
         with self.assertRaises(ImportError):
@@ -93,8 +93,8 @@ class ServerLifecycle(TestCase):
 
 
 class ServerMeta(TestCase):
+    @mock.patch('os.mkdir', lambda p: None)
     @mock.patch('profab.server.EC2Connection', MockConnection)
     @mock.patch('profab.server.regions', regions)
-    @mock.patch('os.mkdir', lambda p: None)
     def test_get_servers(self):
         servers = Server.get_all('test')
