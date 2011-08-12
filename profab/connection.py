1dfeb09a551f630a009e2b0291a3a442c2d73131
Kirit Sælensminde <k@kirit.com>
2011-08-12 16:37:18 +0700

    We can now connect to any of the regions.

--- profab/connection.py
+++ profab/connection.py
@@ -1,5 +1,6 @@
 """Make a connection to a region.
 """
+from boto.ec2 import get_region
 from boto.ec2.connection import EC2Connection
 
 
@@ -9,3 +10,7 @@ def ec2_connect(config):
     bootstrap = EC2Connection(config.keys.api, config.keys.secret)
     if config.region == 'us-east-1':
         return bootstrap
+    region_info = bootstrap.get_all_regions(
+        region_names=[config.region])[0]
+    return region_info.connect(aws_access_key_id=config.keys.api,
+            aws_secret_access_key=config.keys.secret)
