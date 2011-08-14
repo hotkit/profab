8e59c9c85b2a6f9e3c1ee3b831215751a4ebf45b
Kirit Sælensminde <k@kirit.com>
2011-08-14 10:17:48 +0700

    Refactored the Lucid AMI chooser so that we can extend it more easily in the future.

--- profab/role/ami/lucid.py
+++ profab/role/ami/lucid.py
@@ -3,16 +3,29 @@
 from profab.role import Role
 
 
+AMIS = {
+    '32': {
+        'ebs': {
+            'ap-northeast-1' : 'ami-32d36633',
+            'ap-southeast-1' : 'ami-44f18916',
+            'eu-west-1': 'ami-52417126',
+            'us-east-1': 'ami-2cc83145',
+            'us-west-1' : 'ami-95c694d0',
+        },
+        'instance': {
+            'ap-northeast-1' : 'ami-96d06597',
+            'ap-southeast-1' : 'ami-1cf0884e',
+            'eu-west-1': 'ami-945f6fe0',
+            'us-east-1': 'ami-81b275e8',
+            'us-west-1' : 'ami-73c69436',
+        }
+    }
+}
+
 class AddRole(Role):
     """Returns the right Lucid AMI name.
     """
     def ami(self, region):
         """Return the AMI that was passed in to the role.
         """
-        return {
-            'ap-northeast-1' : 'ami-32d36633',
-            'ap-southeast-1' : 'ami-44f18916',
-            'eu-west-1': 'ami-52417126',
-            'us-east-1': 'ami-2cc83145',
-            'us-west-1' : 'ami-95c694d0',
-        }.get(region, None)
+        return AMIS['32']['instance'].get(region, None)
