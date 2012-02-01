5bd3ce59bdb63bdc6d9ad96fd8f3be89cd81326c
Kirit Saelensminde <k@kirit.com>
2012-02-01 16:25:34 +0700

    The new version of Boto (2.2.0) doesn't install properly.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "profab",
-    version = "0.4.2",
+    version = "0.4.3",
     author = "Proteus Technologies Infrastructure team",
     author_email = "infrastructure@proteus-tech.com",
     url = 'https://github.com/Proteus-tech/profab',
@@ -26,7 +26,5 @@ setup(
     scripts = [
         'bin/pf-server-list', 'bin/pf-server-role-add',
         'bin/pf-server-start', 'bin/pf-server-terminate', 'bin/pf-server-upgrade'],
-    install_requires = ['simplejson', 'fabric >= 1.2.0', 'boto >= 2.0'],
-    dependency_links = [
-        'git://github.com/boto/boto.git'],
+    install_requires = ['simplejson', 'fabric >= 1.2.0', 'boto >= 2.0,!=2.2.0'],
 )
