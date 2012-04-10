b9a0c2d92b70be3039d48627615dbaad045b0056
AttakornP <cartoonman_jr@hotmail.com>
2012-04-10 14:26:47 +0700

    modify postgres.py and test

--- profab/role/postgres.py
+++ profab/role/postgres.py
@@ -5,6 +5,7 @@ from fabric.contrib.files import sed, exists
 
 from profab.role import Role
 
+
 class AddRole(Role):
     """Adds the default Postgres configuration.
     """
@@ -27,20 +28,21 @@ class AddRole(Role):
             run('createuser -l -S -D -I -R www-data')
             run('createdb -O www-data -E UTF-8 www-data')
 
-        self.check_version_postgres('/etc/postgresql')
+        postgres_path = self.get_configuration_path('/etc/postgresql')
+        sed('%s/main/pg_hba.conf' % postgres_path,
+            '127.0.0.1/32', '127.0.0.1/16', use_sudo=True)
+        sudo('`ls /etc/init.d/postgres*` restart')
+
 
     @staticmethod
-    def check_version_postgres(postgres_path):
+    def get_configuration_path(postgres_path):
         """Check version of postgres is 8.4 or 9.1 .
         """
         if exists('%s/9.1' % postgres_path):
-            sed('/etc/postgresql/9.1/main/pg_hba.conf',
-                '127.0.0.1/32', '127.0.0.1/16', use_sudo=True)
-            sudo('`ls /etc/init.d/postgres*` restart')
+            postgres_path = '%s/9.1' % postgres_path
         elif exists('%s/8.4' % postgres_path):
-            sed('/etc/postgresql/8.4/main/pg_hba.conf',
-                '127.0.0.1/32', '127.0.0.1/16', use_sudo=True)
-            sudo('`ls /etc/init.d/postgres*` restart')
+            postgres_path = '%s/8.4' % postgres_path
         else:
             msg = '''[Error] Support  postgres version 8.4 and 9.1 only. '''
             raise Exception(msg)
+        return postgres_path
