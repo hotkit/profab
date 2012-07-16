17f627074fae3e8b3e1dedb81c9b98ae6418332b
vernomcrp <vernomcrp@gmail.com>
2012-07-16 20:54:19 +0700

    PINFR269 - move constant to function

--- profab/role/ami/precise.py
+++ profab/role/ami/precise.py
@@ -5,29 +5,40 @@ from profab import _logger
 import urllib2
 import re
 
-raw_pattern=r"<td><p> (\w+-\w+-\d+) </p></td>\n  <td><p> (\d+).+ </p></td>\n  <td><p> (\w+) </p></td>\n.+\n.+ec2-run-instances (\w+-\w+) --instance-type (\w\d+[.]\w+)"
-website="http://ubuntutym2.u-toyama.ac.jp/uec-images/releases/precise/release/"
+
+RAW_PATTERN = r"<td><p> (\w+-\w+-\d+) </p></td>\n  <td><p> (\d+).+</p></td>\n  <td><p> (\w+) </p></td>\n.+\n.+ec2-run-instances (\w+-\w+) --instance-type (\w\d+[.]\w+)"
+WEBSITE = "http://ubuntutym2.u-toyama.ac.jp/uec-images/releases/precise/release/"
+COMPILED_PATTERN = re.compile(RAW_PATTERN)
+
 
 def struct_amis_dict():
     try:
-        response=urllib2.urlopen(website)
-    except HTTPError,e:
+        response = urllib2.urlopen(WEBSITE)
+    except urllib2.HTTPError, e:
         _logger.error(e.msg)
         return None
-    html=response.read()
-    compiled_pattern=re.compile(raw_pattern)
-    ami_tuple_list=compiled_pattern.findall(html)
+    html = response.read()
+    ami_tuple_list = COMPILED_PATTERN.findall(html)
     if ami_tuple_list:
-        tmp={"32":{"ebs":{},"instance":{}},"64":{"ebs":{},"instance":{}}}
+        tmp = {
+            "32": {"ebs": {}, "instance": {}},
+            "64": {"ebs": {}, "instance": {}}
+        }
         for ami_tuple in ami_tuple_list:
-            tmp[ami_tuple[1]][ami_tuple[2]][ami_tuple[0]]=ami_tuple[3]
+            tmp[ami_tuple[1]][ami_tuple[2]][ami_tuple[0]] = ami_tuple[3]
         return tmp
     else:
         return None
 
 
+FRESH_AMIS_DICT = struct_amis_dict()
+
+
 class AddRole(ChooseAMI):
     """Data for Precise AMIs.
     """
-    AMIS=struct_amis_dict()
-
+    def ami(self, region, bits, size):
+        # pylint: disable = E1101
+        disk = 'ebs' if size == 't1.micro' else 'instance'
+        amis = struct_amis_dict()
+        return amis[str(bits)][disk].get(region, None)
