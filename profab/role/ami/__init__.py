25ae6e128f627bfbf00448a76b96d43ce61eef23
Kirit Sælensminde <k@kirit.com>
2013-01-04 16:06:02 +0700

    Removed some dead code.

--- profab/role/ami/__init__.py
+++ profab/role/ami/__init__.py
@@ -19,13 +19,7 @@ class Configure(Role):
 class ChooseAMI(Role):
     """Returns the correct AMI for a given Ubuntu version.
     """
-    def ami(self, region, bits, size):
-        """Return a suitable AMI.
-        """
-        # The AMIS member needs to be provided by the sub-class
-        # pylint: disable = E1101
-        disk = 'ebs' if size == 't1.micro' else 'instance'
-        return self.AMIS[str(bits)][disk].get(region, None)
+    pass
 
 
 RAW_PATTERN = r"""(\w+-\w+-\d+).+\n""" \
