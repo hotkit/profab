b8c80f4153a5ce6bf320152de784bdab797347fa
Kirit Sælensminde <k@kirit.com>
2013-01-04 13:48:41 +0700

    Moved the AMI finding code to a new location.

--- profab/role/ami/__init__.py
+++ profab/role/ami/__init__.py
@@ -1,6 +1,19 @@
 """Plugins that allow the AMI to be chosen.
 """
+import re
+import urllib2
+
+from profab import _logger
 from profab.role import Role
+
+
+RAW_PATTERN = r"""(\w+-\w+-\d+).+\n""" \
+    r"""\s+<td><p>\s+(32|64)-\w+.+\n""" \
+    r"""\s+<td><p>\s+(\w+).+\n""" \
+    r""".+(\w\w\w-\w+)\s+</p></td>$"""
+WEBSITE = "http://ubuntutym2.u-toyama.ac.jp" \
+    "/uec-images/releases/precise/release/"
+COMPILED_PATTERN = re.compile(RAW_PATTERN, re.MULTILINE)
 
 
 class Configure(Role):
@@ -22,4 +35,32 @@ class ChooseAMI(Role):
         # pylint: disable = E1101
         disk = 'ebs' if size == 't1.micro' else 'instance'
         return self.AMIS[str(bits)][disk].get(region, None)
+
+
+def struct_amis_dict():
+    """Download the AMI list HTML and parse it to find the AMI codes
+    for the various versions of Ubuntu.
+    """
+    try:
+        response = urllib2.urlopen(WEBSITE)
+    except urllib2.HTTPError, error:
+        _logger.error(error.msg)
+        return None
+    html = response.read()
+    ami_tuple_list = COMPILED_PATTERN.findall(html)
+    if ami_tuple_list:
+        tmp = {
+            "32": {"ebs": {}, "instance": {}, "hvm": {}},
+            "64": {"ebs": {}, "instance": {}, "hvm": {}},
+        }
+        for ami_tuple in ami_tuple_list:
+            try:
+                tmp[ami_tuple[1]][ami_tuple[2]][ami_tuple[0]] = ami_tuple[3]
+            except:
+                _logger.error(ami_tuple)
+                raise
+        return tmp
+    else:
+        _logger.error("No AMIs found in HTML")
+        return None
 
--- profab/role/ami/precise.py
+++ profab/role/ami/precise.py
@@ -1,43 +1,6 @@
 """Choose the latest AMI for Ubuntu Precise.
 """
-from profab.role.ami import ChooseAMI
-from profab import _logger
-import urllib2
-import re
-
-
-RAW_PATTERN = r"""(\w+-\w+-\d+).+\n""" \
-    """\s+<td><p>\s+(32|64)-\w+.+\n""" \
-    """\s+<td><p>\s+(\w+).+\n""" \
-    """.+(\w\w\w-\w+)\s+</p></td>$"""
-WEBSITE = "http://ubuntutym2.u-toyama.ac.jp" \
-    "/uec-images/releases/precise/release/"
-COMPILED_PATTERN = re.compile(RAW_PATTERN, re.MULTILINE)
-
-
-def struct_amis_dict():
-    try:
-        response = urllib2.urlopen(WEBSITE)
-    except urllib2.HTTPError, e:
-        _logger.error(e.msg)
-        return None
-    html = response.read()
-    ami_tuple_list = COMPILED_PATTERN.findall(html)
-    if ami_tuple_list:
-        tmp = {
-            "32": {"ebs": {}, "instance": {}, "hvm": {}},
-            "64": {"ebs": {}, "instance": {}, "hvm": {}},
-        }
-        for ami_tuple in ami_tuple_list:
-            try:
-                tmp[ami_tuple[1]][ami_tuple[2]][ami_tuple[0]] = ami_tuple[3]
-            except:
-                _logger.error(ami_tuple)
-                raise
-        return tmp
-    else:
-        _logger.error("No AMIs found in HTML")
-        return None
+from profab.role.ami import ChooseAMI, struct_amis_dict
 
 
 class AddRole(ChooseAMI):
@@ -48,3 +11,4 @@ class AddRole(ChooseAMI):
         disk = 'ebs' if size == 't1.micro' else 'instance'
         amis = struct_amis_dict()
         return amis[str(bits)][disk].get(region, None)
+
