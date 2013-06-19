5ba46a9c45d360da75bc7774348bc67af10750b2
Kirit Sælensminde <k@kirit.com>
2013-06-19 14:49:20 +0700

    Factor out the Postgres role adding so other roles can be easily added.

--- profab/role/postgres.py
+++ profab/role/postgres.py
@@ -12,9 +12,23 @@ class AddRole(Role):
     packages = ['postgresql']
 
 
+    @classmethod
+    def ident_user(cls, _server, username):
+        """Make sure that the specified user name ihas a Postgres role
+        and user database.
+        """
+        pguser = \
+            run('''psql -x -c "select * from pg_user where username='%s'"'''
+                % username)
+        if 'No rows' in pguser:
+            run('createuser -l -S -D -I -R %s' % username)
+            run('createdb -O %s -E UTF-8 %s' % (username, username))
+
+
     def configure(self, server):
         """Configure Postgres once it is running.
         """
+        # The first user needs to be created using sudo
         pguser = sudo('''psql -x -c "select * from pg_user '''
             '''where usename='ubuntu'"''', user='postgres')
         if 'No rows' in pguser:
@@ -22,11 +36,8 @@ class AddRole(Role):
                 user='postgres')
             sudo('psql -c "create database ubuntu with owner=ubuntu"',
                 user='postgres')
-        pguser = run('''psql -x -c "select * from pg_user where '''
-            '''usename='www-data'"''')
-        if 'No rows' in pguser:
-            run('createuser -l -S -D -I -R www-data')
-            run('createdb -O www-data -E UTF-8 www-data')
+        # Other users can be created generically
+        self.ident_user(server, 'www-data')
 
         postgres_path = self.get_configuration_path('/etc/postgresql')
         sed('%s/main/pg_hba.conf' % postgres_path,
